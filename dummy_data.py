180888ac797e7583684494ac7167f7c4ae9f65a2
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-11-27 19:57:31 +0200

    add dummy data for products

--- dummy_data.py
+++ dummy_data.py
@@ -19,7 +19,26 @@ def seed_brand(n):
 
 
 def seed_product(n):
-    pass
+    fake = Faker()
+    images = ['1.jpg','2.jpg','3.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','14.jpg']
+    flags = ['New','Sale','Feature']
+    for _ in range(n):
+        Product.objects.create(
+            name = fake.name(),
+            image = f'brands/{images[random.randint(0,11)]}',
+            flag = flags[random.randint(0,2)],
+            price = round(random.uniform(20.99,999.99),2),
+            sku = random.randint(1000,1000000),
+            subtitle = fake.text(max_nb_chars=250),
+            description = fake.text(max_nb_chars=20000),
+            quantity = random.randint(0,100),
+            brand = Brand.objects.get(id=random.randint(1,160)),
+        )
+    print(f'seed {n} Products Successfully')
 
 
-seed_brand(5)
+
+
+#seed_brand(150)
+
+seed_product(350)
