b3798bdb3c06938b32f2d108cf08c39218ae1ed6
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-12-06 21:59:53 +0200

    add pagination

--- product/views.py
+++ product/views.py
@@ -104,6 +104,7 @@ class ProductDetail(DetailView):
 class BrandList(ListView):
     model = Brand
     queryset = Brand.objects.annotate(product_count=Count('product_brand'))
+    paginate_by = 20
 
 
 
