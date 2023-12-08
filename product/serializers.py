a6f590492f6c68950d96dfbb30127804bb7fc1f1
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-12-08 17:43:20 +0200

    add brand in product list , add reviews_count & avg_rate in product detail

--- product/serializers.py
+++ product/serializers.py
@@ -4,7 +4,17 @@ from django.db.models.aggregates import Avg
 
 
 
+
+class BrandListSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+
+
+
 class ProductListSerialzer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
     avg_rate = serializers.SerializerMethodField()
     reviews_count = serializers.SerializerMethodField()
 
@@ -27,17 +37,24 @@ class ProductListSerialzer(serializers.ModelSerializer):
 
 
 class ProductDetailSerialzer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
+    avg_rate = serializers.SerializerMethodField()
+    reviews_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
 
-
-
-
-class BrandListSerialzer(serializers.ModelSerializer):
-    class Meta:
-        model = Brand
-        fields = '__all__'
+    def get_avg_rate(self,product):
+        avg = product.review_product.aggregate(rate_avg=Avg('rate'))
+        if not avg['rate_avg']:
+            result = 0
+            return result
+        return avg['rate_avg']
+    
+    def get_reviews_count(self,product:Product):
+        reviews = product.review_product.all().count()
+        return reviews 
 
 
 
