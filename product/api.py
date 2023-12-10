1f389468c00b4018927e705852feaf9a0243e215
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-12-10 20:07:48 +0200

    add search , order , filter

--- product/api.py
+++ product/api.py
@@ -1,6 +1,8 @@
 # from rest_framework.decorators import api_view
 # from rest_framework.response import Response
 from .serializers import ProductListSerializer , ProductDetailSerializer , BrandListSerializer , BrandDetailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from .models import Product , Brand
 from rest_framework import generics
 
@@ -26,6 +28,10 @@ from rest_framework import generics
 class ProductListAPI(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    filterset_fields = ['flag' ,'brand']
+    search_fields = ['name' ,'subtitle' ,'description']
+    ordering_fields = ['price', 'quantity']
 
 
 
