738ff30d4f6c93b5ede7a9e546334668a41c1aed
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2024-01-30 15:50:00 +0200

    add translation part 1

--- project/settings.py
+++ project/settings.py
@@ -74,6 +74,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'allauth.account.middleware.AccountMiddleware',
@@ -147,7 +148,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar'
 
 TIME_ZONE = 'UTC'
 
@@ -180,3 +181,11 @@ REST_AUTH = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+LOCALE_PATHS = ['locale']
+
+LANGUAGES = [
+    ("ar",("Arabic")),
+    ("en",("English")),
+]
